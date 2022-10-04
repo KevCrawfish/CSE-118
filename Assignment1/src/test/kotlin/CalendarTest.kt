d878d820a2108cc1e94cc82f47e7ce244eb631eb
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-10-03 22:57:03 -0700

    final push for real

--- Assignment1/src/test/kotlin/CalendarTest.kt
+++ Assignment1/src/test/kotlin/CalendarTest.kt
@@ -18,23 +18,29 @@ class CalendarTest {
     val days = Calendar().generate(YearMonth.of(2026, 2))
     val prev = days[0][0]
     val last = days[3][6]
+    val next = days[5][6]
     assertEquals(28, last)
     assertEquals(1, prev)
+    assertEquals(14, next)
   }
   @Test
   internal fun `September 2022 test`() {
     val days = Calendar().generate(YearMonth.of(2022, 9))
     val prev = days[0][3]
     val last = days[4][5]
+    val next = days[5][6]
     assertEquals(30, last)
     assertEquals(31, prev)
+    assertEquals(8, next)
   }
   @Test
   internal fun `March 2025 test`() {
     val days = Calendar().generate(YearMonth.of(2025, 3))
     val prev = days[0][5]
     val last = days[5][1]
+    val next = days[5][6]
     assertEquals(31, last)
     assertEquals(28, prev)
+    assertEquals(5, next)
   }
 }
