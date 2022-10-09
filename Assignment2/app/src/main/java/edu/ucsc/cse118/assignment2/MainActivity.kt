1ad9b3897ea1dfcdcee6fe73219cef1c2c501e85
KevCrawfish <78246147+KevCrawfish@users.noreply.github.com>
2022-10-09 14:38:07 -0700

    Starter files

---
+++ Assignment2/app/src/main/java/edu/ucsc/cse118/assignment2/MainActivity.kt
@@ -0,0 +1,10 @@
+package edu.ucsc.cse118.assignment2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
